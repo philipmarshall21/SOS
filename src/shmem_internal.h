92d2892b43033a5b0956832c9c8992bad467b658
Philip Marshall <philip.marshall@intel.com>
2024-03-15 11:41:07 -0700

    Correct the implementation of SHMEM_ERR_CHECK_SYMMETRIC

--- src/shmem_internal.h
+++ src/shmem_internal.h
@@ -275,12 +275,14 @@ extern hwloc_topology_t shmem_internal_topology;
         else if (shmem_external_heap_base) {                                            \
             const void *heap_ext_external = (void*)((uint8_t *) shmem_external_heap_base +                             \
                                                    shmem_external_heap_length);                                        \
-            if (ptr_base >= shmem_external_heap_base && ptr_base < heap_ext_external && ptr_ext > heap_ext_external) { \
-                RAISE_ERROR_MSG("Argument \"%s\" [%p..%p) exceeds device "                                             \
-                                "sym. heap region [%p..%p)\n", #ptr_in, ptr_base,                                      \
-                                ptr_ext, shmem_external_heap_base, heap_ext_external);                                 \
-            }                                                                                                          \
-        }                                                                                                              \
+            if (ptr_base >= shmem_external_heap_base && ptr_base < heap_ext_external) {     \
+                if (ptr_ext > heap_ext_external) {                                          \
+                    RAISE_ERROR_MSG("Argument \"%s\" [%p..%p) exceeds device "              \
+                                    "sym. heap region [%p..%p)\n", #ptr_in, ptr_base,       \
+                                    ptr_ext, shmem_external_heap_base, heap_ext_external);  \
+                }                                                                       \
+            }                                                                           \
+        }                                                                               \
         else {                                                                          \
             RAISE_ERROR_MSG("Argument \"%s\" is not symmetric (%p)\n",                  \
                             #ptr_in, ptr_base);                                         \
